12d502d1e01ba0a772e69f0dcb665e71ddeaa4f7
PhillipMagos <phillipmagos@gmail.com>
2023-07-11 23:16:02 -0500

    first push

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold.mb-5">Home</h1>
+      <p className="mb-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed dolores blanditiis, maiores tenetur eius optio. Consequatur eligendi iusto, quisquam perspiciatis quos repudiandae quaerat suscipit vitae molestias illum, quas quidem voluptatem.
+      </p>
+    </>
+  );
+}
